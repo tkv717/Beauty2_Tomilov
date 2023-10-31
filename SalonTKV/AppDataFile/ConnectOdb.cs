452a53c57cc8f07baf5e46099de264441a73ac9d
exxsize79 <99488943+tkv717@users.noreply.github.com>
2023-10-31 14:49:27 +0300

    1
    1

---
+++ SalonTKV/AppDataFile/ConnectOdb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonTKV.AppDataFile
+{
+    internal class ConnectOdb
+    {
+        public static St2_Tomilov_Salon1Entities1 conObj;
+    }
+}
